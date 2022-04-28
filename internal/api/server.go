974381a01fa94a9b9013af4a4dcde83093df93e7
maximilian@fanbase.com <maximilian@fanbase.com>
2022-04-28 02:32:24 +0200

    dev

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,11 +1,9 @@
 package core
 
 import (
+	"encoding/json"
 	"net/http"
-	"reflect"
 	"regexp"
-
-	"github.com/jmoiron/sqlx"
 )
 
 var (
@@ -13,21 +11,82 @@ var (
 	matchMethodName     = regexp.MustCompile(`^(.+)V([0-9]+)$`)
 )
 
-// Server ...
-type Server struct {
-	systems map[reflect.Type]interface{}
-	dbh     *sqlx.DB
-}
-
-// NewServer returns a new Server
-func NewServer(dbh *sqlx.DB) *Server {
-	return &Server{
-		systems: map[reflect.Type]interface{}{},
-		dbh:     dbh,
+func (h *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	switch {
+	case r.Method == http.MethodGet:
+		h.List(w, r)
+		return
+	case r.Method == http.MethodGet:
+		h.Get(w, r)
+		return
+	case r.Method == http.MethodPost:
+		h.Create(w, r)
+		return
+	default:
+		notFound(w, r)
+		return
 	}
 }
 
-// ListenAndServe will start listening on http on the given addr
-func (s *Server) ListenAndServe(addr string) error {
-	return http.ListenAndServe(addr, nil)
+func (h *userHandler) List(w http.ResponseWriter, r *http.Request) {
+	h.store.RLock()
+	users := make([]user, 0, len(h.store.m))
+	for _, v := range h.store.m {
+		users = append(users, v)
+	}
+	h.store.RUnlock()
+	jsonBytes, err := json.Marshal(users)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Get(w http.ResponseWriter, r *http.Request) {
+	h.store.RLock()
+	u, ok := h.store.m[matches[1]]
+	h.store.RUnlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("user not found"))
+		return
+	}
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var u user
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		internalServerError(w, r)
+		return
+	}
+	h.store.Lock()
+	h.store.m[u.ID] = u
+	h.store.Unlock()
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("internal server error"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
 }
